90db31d2f3b418d310e43f1ed33f296c25ce054f
rfontaine974 <rfontaine.n3@gmail.com>
2025-12-03 16:27:02 +0100

    Add config.js with WEB_APP_URL

---
+++ config.js
@@ -0,0 +1,3 @@
+// Fichier généré automatiquement par l'assistant
+// Contient l'URL du backend Apps Script utilisé par l'application
+window.__CONFIG__ = { WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxoc_EOEA57sWlVlI9UiRl0hxyrnY-HBGWkwObVYluvFAUw9ZtYuhLIkU0Vu9dHs-IE/exec' };
